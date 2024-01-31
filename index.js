40c6d458cfa78599267ba992397c7be06cea7d54
Abhi10-G <abhinitig2020csbs@gmail.com>
2024-01-31 21:20:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
+
+const db=new pg.Client(
+    {
+        host:"localhost",
+        port: 5432,
+        password:"Abhi",
+        user:'postgres',
+        database:'ToDoList'
+    }
+)
+db.connect()
+
+const server=express();
+server.use(bodyParser.urlencoded({extended: false}))
+
+server.get("/",async(req,res)=>
+{
+    const dbtodo=await db.query("SELECT * FROM public.todo ORDER BY id ASC ")
+    res.render("index.ejs",{todos: dbtodo.rows})
+}
+)
+server.post("/add", async (req, res) => {
+    const todo = req.body.todo
+    await db.query(`INSERT INTO todo (task) VALUES ('${todo}')`)
+    res.redirect('/')
+  })
+
+server.listen(3000,()=>
+{
+    console.log("Server Started");
+})
